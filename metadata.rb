84d5071411670cb2f44b19953ece3b30a6e3cfd9
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2013-12-27 15:04:22 -0500

    Added dependencies to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
-name             'selenium'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+name             'selenium-grid'
+maintainer       'Quoin Inc.'
+maintainer_email 'pavel.nabutovsky@quoininc.com'
 license          'All rights reserved'
-description      'Installs/Configures selenium'
+description      'Installs/Configures Selenium'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
+
+
+depends 'firefox'
+depends 'xvfb'
+depends 'x11vnc'
+depends 'java'
