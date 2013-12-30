cb7b902fc635150e5c0791204c30231c95cbca77
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2013-12-30 17:33:21 -0500

    Cleaned up metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,10 @@ description      'Installs/Configures Selenium'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
+supports 'debian'
+supports 'ubuntu'
 
-depends 'firefox'
 depends 'xvfb'
 depends 'x11vnc'
 depends 'java'
+depends 'quoin'
