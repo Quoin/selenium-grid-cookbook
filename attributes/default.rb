998dd4919431ad250e578d0881df7b9006872c08
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2013-12-31 09:26:05 -0500

    Added extra parameters for setting the Selenium hub port.

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,14 @@ default['selenium-grid']['version'] = "2.35.0"
 default['selenium-grid']['server-jar'] = "selenium-server-standalone-#{node['selenium-grid']['version']}.jar"
 default['selenium-grid']['server-jar-url'] = "https://selenium.googlecode.com/files/#{node['selenium-grid']['server-jar']}"
 
+default['selenium-grid']['hub']['host'] = "localhost"
+default['selenium-grid']['hub']['port'] = "4444"
+default['selenium-grid']['hub']['url'] = "http://#{node['selenium-grid']['hub']['host']}:#{node['selenium-grid']['hub']['port']}"
+
+
+default['selenium-grid']['firefox']['port'] = "5555"
+default['selenium-grid']['chrome']['port'] = "5556"
+
 default['selenium-grid']['chromedriver']['architecture'] = "linux64"
 default['selenium-grid']['chromedriver']['version'] = "2.3"
 
