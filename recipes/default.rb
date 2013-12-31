20f4335e7c8866187d641a67109484c32e3026ef
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2013-12-31 09:34:01 -0500

    Default recipe launching hub.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,11 @@
 #
-# Cookbook Name:: selenium
+# Cookbook Name:: selenium-grid
 # Recipe:: default
 #
 # Copyright 2013, Quoin Inc.
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe 'selenium-grid::hub'
+#include_recipe 'selenium-grid::firefox'
+#include_recipe 'selenium-grid::chrome'
